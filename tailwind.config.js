7230389778768950766bb3f1fe221fc66e23ec55
meqdad <meqdad.moradi@outlook.com>
2022-02-06 22:52:10 +0100

    all files and folders added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   content: ["./dist/**/*.{html,js}"],
+   theme: {
+      extends: {},
+      container: {
+         center: true,
+         padding: "1rem",
+      },
+
+      fontFamily: {
+         Poppins: "Poppins , sans-serif",
+      },
+   },
+   plugins: [],
+};
